1880eed44ea6e0693537be8a3cf392324c88b4c3
philippeloe <163553903+philippeloe@users.noreply.github.com>
2024-05-29 21:27:10 +0200

    Update __main__.py

--- src/moser/__main__.py
+++ src/moser/__main__.py
@@ -1,1 +1,15 @@
-from .moser import main_moser
+#from .moser import main_moser
+
+from BalanceEq import *
+from Concentration import *
+from Decompose import *
+from HConcentration import *
+from MolarMass import *
+from Reaction_constant_activity import *
+from Reaction_constant_concentration import *
+from Titration import *
+from calculate_speed import *
+from instantaneous_speed import *
+from main_moser import *
+from reaction_order import *
+from read_file_and_enter_data import *
