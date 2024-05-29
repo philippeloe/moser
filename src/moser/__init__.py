a1f39c91658e112900398c888cdb5e394d2aefc1
philippeloe <163553903+philippeloe@users.noreply.github.com>
2024-05-29 21:11:19 +0200

    Update __init__.py

--- src/moser/__init__.py
+++ src/moser/__init__.py
@@ -1,5 +1,19 @@
 """MOSER.py (for Molecular Operations and Solutions for Equilibria and Reactions) is a versatile Python package designed for physical chemistry calculations. It enables users to balance chemical equations, trace titration curves, calculate solutions' concentrations and pH, determine molar masses, evaluate reactional quotients, and analyze reaction kinetics efficiently and accurately.."""
 
-from .moser import *
+#from .moser import *
+
+from BalanceEq import *
+from Concentration import *
+from Decompose import *
+from HConcentration import *
+from MolarMass import *
+from Reaction_constant_activity import *
+from Reaction_constant_concentration import *
+from Titration import *
+from calculate_speed import *
+from instantaneous_speed import *
+from main_moser import *
+from reaction_order import *
+from read_file_and_enter_data import *
 
 __version__ = "0.0.1"
